e0e44f33184ba240b9190b246fd0b4e1c41a1b56
Icegreeen <flavioaquila16@gmail.com>
2021-08-09 07:53:30 -0300

    finalized project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './components/Home';
+import NasaPhoto from './components/NasaPhoto';
+
+import './styles/Home.css';
+
+export default function App(){
+  return (
+    <BrowserRouter>
+      <div className="app">
+          <Route component={Home} path="/" exact />
+          <Route component={NasaPhoto} path="/nasaphoto" />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+
